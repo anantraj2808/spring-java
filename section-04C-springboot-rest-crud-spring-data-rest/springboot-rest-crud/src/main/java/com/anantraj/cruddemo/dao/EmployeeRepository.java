f7f30885908240cbf7adedf0bae210bec586f5b4
anantraj2808 <anantaditya6@gmail.com>
2023-12-24 16:41:52 +0530

    spring data configs

--- section-04C-springboot-rest-crud-spring-data-rest/springboot-rest-crud/src/main/java/com/anantraj/cruddemo/dao/EmployeeRepository.java
+++ section-04C-springboot-rest-crud-spring-data-rest/springboot-rest-crud/src/main/java/com/anantraj/cruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.anantraj.cruddemo.dao;
 
 import com.anantraj.cruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//For changing endpoint
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //Jpa gives pre defined generic functions
 }
